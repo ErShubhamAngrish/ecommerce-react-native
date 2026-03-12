bbf00ad08aea8ba3bd6695300375ef6538f2b9eb
ErShubhamAngrish <ershubhamsharma@yahoo.com>
2026-03-12 17:56:10 +0530

    ProductCard Component Created

--- App.tsx
+++ App.tsx
@@ -12,6 +12,7 @@ import {
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 import { QueryClient,QueryClientProvider } from '@tanstack/react-query';
+import RootNavigation from './src/navigation';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -21,7 +22,8 @@ function App() {
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <QueryClientProvider client={queryClient}>
-
+        <RootNavigation>
+        </RootNavigation>
       </QueryClientProvider>
     </SafeAreaProvider>
   );
---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,16 @@
+import { Image, Text, View } from 'react-native';
+
+const ProductCard = () => {
+  return (
+    <View>
+      <Image></Image>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default ProductCard;
